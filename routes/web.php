f92ffeee775425aaf803e27e31312513851dea8c
felipeatari <mr.robot.felipe@gmail.com>
2024-05-23 18:09:45 -0300

    Implementa layout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\AdminProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Default
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/product', [\App\Http\Controllers\ProductController::class, 'show']);
+
+// Admin
+Route::get('/admin/products', [AdminProductController::class, 'index']);
+Route::get('/admin/products/edit', [AdminProductController::class, 'edit']);

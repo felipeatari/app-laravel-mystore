44e6c700e2a1427bf90434b0987cdd98f72de271
felipeatari <mr.robot.felipe@gmail.com>
2024-05-24 01:47:21 -0300

    Produto - Finaliza cadastrar

--- app/Http/Controllers/AdminProductController.php
+++ app/Http/Controllers/AdminProductController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Str;
 
 class AdminProductController extends Controller
 {
@@ -29,6 +31,26 @@ class AdminProductController extends Controller
         return view('admin.products_create');
     }
 
-    public function create_req()
-    {}
+    public function create_req(Request $request)
+    {
+        $inputs = $request->validate([
+            'name' => 'string|required',
+            'price' => 'string|required',
+            'stock' => 'integer|nullable',
+            'cover' => 'file|nullable',
+            'description' => 'string|nullable',
+        ]);
+
+        $inputs['slug'] = Str::slug($inputs['name']);
+
+        if (!empty($inputs['cover']) && $inputs['cover']->isValid()) {
+            $file = $inputs['cover'];
+            $path = $file->store('products');
+            $inputs['cover'] = $path;
+        };
+
+        Product::create($inputs);
+
+        return Redirect::route('admin.products');
+    }
 }
